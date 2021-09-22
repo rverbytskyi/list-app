06de8554a196e7498c88d7df74b66339d9e2ab76
rverbytskyi <r.j.verbytskyi@gmail.com>
2021-09-23 01:23:39 +0200

    Configure tests

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'react-native',
+  // transformIgnorePatterns: [
+  //   '/node_modules/(?!(react-native-navigation)/)'
+  // ],
+  setupFilesAfterEnv: [
+    '<rootDir>/unit-tests-setup.js',
+    "@testing-library/jest-native/extend-expect",
+  ],
+}
